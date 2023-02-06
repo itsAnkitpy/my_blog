2f32a7b0cd5ff396719bac59e04eea44cb438f98
itsAnkitpy <thegoat3585@gmail.com>
2023-02-06 14:14:22 +0530

    Working with Comments Form

--- blog_app/views.py
+++ blog_app/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render,get_object_or_404
-from .models import *
 from django.views.generic import ListView,DetailView
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+from django.views import View
+
+from .models import *
+from .forms import CommentForm
 
 class IndexPageView(ListView):
     template_name = 'blog_app/index.html'
@@ -19,12 +24,37 @@ class AllPostsView(ListView):
     ordering = ['-date']
     context_object_name = 'all_posts'
 
-class SinglePostView(DetailView):
-    template_name = 'blog_app/post-detail.html'
-    model = Post
+class SinglePostView(View):
+    
+    def get(self,request,slug):
+        post = Post.objects.get(slug=slug)
 
-    def get_context_data(self, **kwargs):
-        context =  super().get_context_data(**kwargs)
-        context['post_tags'] = self.object.tags.all()
-        return context
+        context = {
+            'post':post,
+            'post_tags':post.tags.all(),
+            'comment_form': CommentForm(),
+            'comments':post.comments.all().order_by('-id'),
+        }
 
+        return render(request, 'blog_app/post-detail.html',context)
+
+    def post(self,request,slug):
+        comment_form = CommentForm(request.POST)
+        post = Post.objects.get(slug=slug)
+
+        if comment_form.is_valid():
+            comment = comment_form.save(commit=False)
+            comment.post = post
+            comment.save()
+            return HttpResponseRedirect(reverse('post_detail', args=[slug]))
+
+        
+
+        context = {
+            'post':post,
+            'post_tags':post.tags.all(),
+            'comment_form': comment_form,
+            'comments':post.comments.all().order_by('-id'),
+        }
+
+        return render(request, 'blog_app/post-detail.html',context)
